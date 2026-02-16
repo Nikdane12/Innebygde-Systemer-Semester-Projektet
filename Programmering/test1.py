461c16dbdc0f7034ad67ef3c536efd66c659165b
Olav0MegatroN <mytdv87sd4@privaterelay.appleid.com>
2026-02-17 00:12:52 +0100

    Test me gui

--- Programmering/test1.py
+++ Programmering/test1.py
@@ -1,15 +1,34 @@
-import RPi.GPIO as GPIO
-import time
+from tkinter import * 
+from tkinter import messagebox
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
 
-GPIO.setmode(GPIO.BOARD)
-PIN = 33
-GPIO.setup(PIN, GPIO.OUT)
 
-try:
-    while True:
-        GPIO.output(PIN, 1)
-        time.sleep(0.5)
-        GPIO.output(PIN, 0)
-        time.sleep(0.5)
-finally:
-    GPIO.cleanup()
+# create root window
+root = Tk()
+
+# root window title and dimension
+root.title("Welcome to GeekForGeeks")
+# Set geometry(widthxheight)
+root.geometry('350x200')
+
+# adding a label to the root window
+lbl = Label(root, text = "Are you a Geek?")
+lbl.grid()
+
+# function to display text when
+# button is clicked
+def clicked():
+    messagebox.showinfo("showinfo", "Information") 
+    print(f"Hello world")
+
+# button widget with red color text
+# inside
+btn = Button(root, text = "Click me" ,
+             fg = "red", command=clicked)
+# set Button grid
+btn.grid(column=1, row=0)
+
+# Execute Tkinter
+root.mainloop()
