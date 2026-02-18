991863cbcc22f153c73d7a360fd9efc6f78b644d
Olav0MegatroN <mytdv87sd4@privaterelay.appleid.com>
2026-02-18 14:12:54 +0100

    Update: Test

--- Programmering/test.py
+++ Programmering/test.py
@@ -1,12 +1,18 @@
 from gpiozero import Servo
 from time import sleep
 
-servo = Servo(13)
+servo1 = Servo(18)  # GPIO 18 (pin 12)
+servo2 = Servo(19)  # GPIO 19 (pin 35)
 
 while True:
-	servo.min()
-	sleep(1)
-	servo.mid()
-	sleep(1)
-	servo.max()
-	sleep(1)
+    servo1.min()
+    servo2.min()
+    sleep(1)
+
+    servo1.mid()
+    servo2.mid()
+    sleep(1)
+
+    servo1.max()
+    servo2.max()
+    sleep(1)
